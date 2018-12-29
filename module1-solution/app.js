915fe18c1c4c23b460b0ce0f1a2bdf758f7ce4a3
Collins, Andrew <COLLIA5@nationwide.com>
2018-12-28 23:34:06 -0500

    completed assignment1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,9 +6,33 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
+  $scope.message = "";
+  $scope.dishes = "";
 
-};
+  $scope.checkItems = function () {
+    var count = 0;
+    count = addUpDishes($scope.dishes);
 
+    if ($scope.dishes == "") {
+      $scope.message = "Please enter data first";
+    }
+    else if (count <= 3) {
+        $scope.message = "Enjoy!";
+    }
+    else if (count > 3 ) {
+      $scope.message = "Too much!";
+    }
+    else {
+      $scope.message = "invalid input, you shouldn't have gotten here";
+    }
+  };
 
+  // this adds up the total number of dishes
+  function addUpDishes(string) {
+    var arrayOfDishes = string.split(',');
+    var totalDishesCount = arrayOfDishes.length;
+    return totalDishesCount;
+  }
 }
+
 })();
